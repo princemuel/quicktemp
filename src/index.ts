f0cf0f2b1cd4c2c9a6ce4fdf48ec79d6bacb15ef
pHoeniX-svg <vansomecsam@gmail.com>
2022-05-16 18:08:08 +0100

    getting books and a single book

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { Db } from 'mongodb';
+import { Collection, Db, ObjectId } from 'mongodb';
 import { connectToDb, getDb } from './db';
 import { IBook, IBooks } from './types';
 
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3500;
 
 // db connection
 let db: Db;
+let booksCollection: Collection<IBook>;
 
 connectToDb((err) => {
   if (!err) {
@@ -15,13 +16,14 @@ connectToDb((err) => {
       console.log(`app listening on port ${PORT}`);
     });
     db = getDb();
+    booksCollection = db.collection('books');
   }
 });
 
 app.get('/books', (req, res) => {
   let books: IBooks = [];
 
-  db.collection<IBook>('books')
+  booksCollection
     .find()
     .sort({ author: 1 })
     // @ts-expect-error
@@ -29,7 +31,26 @@ app.get('/books', (req, res) => {
     .then(() => {
       res.status(200).json(books);
     })
-    .catch(() => {
+    .catch((err) => {
       res.status(500).json({ error: 'could not fetch the documents' });
     });
+});
+
+app.get('/books/:id', (req, res) => {
+  const { id } = req.params;
+
+  // NOTE:  find a way to check if the id is a valid BSON ObjectId
+  // but is not present in the database
+  if (ObjectId.isValid(id)) {
+    booksCollection
+      .findOne({ _id: new ObjectId(id) })
+      .then((book) => {
+        res.status(200).json(book);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: 'could not fetch the document' });
+      });
+  } else {
+    res.status(500).json({ error: 'the document id is not a valid id' });
+  }
 });
