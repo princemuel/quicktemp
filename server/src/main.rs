1ad6722f5c9832ec09859ddfdcdf9ae4dc1a63ca
Prince Muel <vansomecsam@gmail.com>
2024-02-10 02:58:33 +0100

    fic: update port number
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- server/src/main.rs
+++ server/src/main.rs
@@ -27,7 +27,7 @@ async fn main() -> std::io::Result<()> {
 
         App::new().wrap(cors).service(convert)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
