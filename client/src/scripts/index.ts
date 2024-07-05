30ed4f7cb988723790f18157d5b5b1481e89d662
Prince Muel <vansomecsam@gmail.com>
2024-07-05 17:51:38 +0100

    feat: get and parse valuse from form

--- client/src/scripts/index.ts
+++ client/src/scripts/index.ts
@@ -1,1 +1,43 @@
-console.log("hello");
+import { Temperature, Unit } from "quicktemp";
+import { getElement } from "./dom";
+
+// Construct the temperature, and get its value, from and to.
+const temperature = Temperature.new();
+
+const form = getElement("form", HTMLFormElement);
+const output = getElement("#result", HTMLOutputElement);
+
+type Values = { from_scale: string; to_scale: string; degree: string };
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const formData = Object.fromEntries(new FormData(form).entries()) as Values;
+
+  try {
+    temperature.set_from(to_unit(formData.from_scale));
+    temperature.set_to(to_unit(formData.to_scale));
+    temperature.set_value(parseFloat(formData.degree));
+
+    output.textContent = `${temperature.convert()}`;
+  } catch (error) {
+    temperature.set_from(to_unit("Celsius"));
+    temperature.set_to(to_unit("Fahrenheit"));
+    temperature.set_value(parseFloat("100"));
+
+    output.textContent = `${temperature.convert()}`;
+  }
+});
+
+function to_unit(value: string) {
+  switch (value.toLowerCase()) {
+    case "celsius":
+      return Unit.Celsius;
+    case "fahrenheit":
+      return Unit.Fahrenheit;
+    case "kelvin":
+      return Unit.Kelvin;
+    default:
+      throw new Error("Unknown unit");
+  }
+}
