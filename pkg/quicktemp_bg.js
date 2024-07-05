7c5304aebd0dad1bb2e1c65b654a793c063ecbdb
Prince Muel <vansomecsam@gmail.com>
2024-07-05 19:46:19 +0100

    fix: resolve issue with deployment, add wasm code

--- pkg/quicktemp_bg.js
+++ pkg/quicktemp_bg.js
@@ -4,6 +4,26 @@ export function __wbg_set_wasm(val) {
 }
 
 
+const heap = new Array(128).fill(undefined);
+
+heap.push(undefined, null, true, false);
+
+function getObject(idx) { return heap[idx]; }
+
+let heap_next = heap.length;
+
+function dropObject(idx) {
+    if (idx < 132) return;
+    heap[idx] = heap_next;
+    heap_next = idx;
+}
+
+function takeObject(idx) {
+    const ret = getObject(idx);
+    dropObject(idx);
+    return ret;
+}
+
 const lTextDecoder = typeof TextDecoder === 'undefined' ? (0, module.require)('util').TextDecoder : TextDecoder;
 
 let cachedTextDecoder = new lTextDecoder('utf-8', { ignoreBOM: true, fatal: true });
@@ -23,6 +43,82 @@ function getStringFromWasm0(ptr, len) {
     ptr = ptr >>> 0;
     return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
 }
+
+function addHeapObject(obj) {
+    if (heap_next === heap.length) heap.push(heap.length + 1);
+    const idx = heap_next;
+    heap_next = heap[idx];
+
+    heap[idx] = obj;
+    return idx;
+}
+
+let WASM_VECTOR_LEN = 0;
+
+const lTextEncoder = typeof TextEncoder === 'undefined' ? (0, module.require)('util').TextEncoder : TextEncoder;
+
+let cachedTextEncoder = new lTextEncoder('utf-8');
+
+const encodeString = (typeof cachedTextEncoder.encodeInto === 'function'
+    ? function (arg, view) {
+    return cachedTextEncoder.encodeInto(arg, view);
+}
+    : function (arg, view) {
+    const buf = cachedTextEncoder.encode(arg);
+    view.set(buf);
+    return {
+        read: arg.length,
+        written: buf.length
+    };
+});
+
+function passStringToWasm0(arg, malloc, realloc) {
+
+    if (realloc === undefined) {
+        const buf = cachedTextEncoder.encode(arg);
+        const ptr = malloc(buf.length, 1) >>> 0;
+        getUint8Memory0().subarray(ptr, ptr + buf.length).set(buf);
+        WASM_VECTOR_LEN = buf.length;
+        return ptr;
+    }
+
+    let len = arg.length;
+    let ptr = malloc(len, 1) >>> 0;
+
+    const mem = getUint8Memory0();
+
+    let offset = 0;
+
+    for (; offset < len; offset++) {
+        const code = arg.charCodeAt(offset);
+        if (code > 0x7F) break;
+        mem[ptr + offset] = code;
+    }
+
+    if (offset !== len) {
+        if (offset !== 0) {
+            arg = arg.slice(offset);
+        }
+        ptr = realloc(ptr, len, len = offset + arg.length * 3, 1) >>> 0;
+        const view = getUint8Memory0().subarray(ptr + offset, ptr + len);
+        const ret = encodeString(arg, view);
+
+        offset += ret.written;
+        ptr = realloc(ptr, len, offset, 1) >>> 0;
+    }
+
+    WASM_VECTOR_LEN = offset;
+    return ptr;
+}
+
+let cachedInt32Memory0 = null;
+
+function getInt32Memory0() {
+    if (cachedInt32Memory0 === null || cachedInt32Memory0.byteLength === 0) {
+        cachedInt32Memory0 = new Int32Array(wasm.memory.buffer);
+    }
+    return cachedInt32Memory0;
+}
 /**
 */
 export const Unit = Object.freeze({ Celsius:0,"0":"Celsius",Fahrenheit:1,"1":"Fahrenheit",Kelvin:2,"2":"Kelvin", });
@@ -33,6 +129,14 @@ const TemperatureFinalization = (typeof FinalizationRegistry === 'undefined')
 /**
 */
 export class Temperature {
+
+    static __wrap(ptr) {
+        ptr = ptr >>> 0;
+        const obj = Object.create(Temperature.prototype);
+        obj.__wbg_ptr = ptr;
+        TemperatureFinalization.register(obj, obj.__wbg_ptr, obj);
+        return obj;
+    }
 
     __destroy_into_raw() {
         const ptr = this.__wbg_ptr;
@@ -45,7 +149,101 @@ export class Temperature {
         const ptr = this.__destroy_into_raw();
         wasm.__wbg_temperature_free(ptr);
     }
+    /**
+    * @returns {Temperature}
+    */
+    static new() {
+        const ret = wasm.temperature_new();
+        return Temperature.__wrap(ret);
+    }
+    /**
+    * @returns {number}
+    */
+    convert() {
+        const ret = wasm.temperature_convert(this.__wbg_ptr);
+        return ret;
+    }
+    /**
+    * @returns {number}
+    */
+    get_degree() {
+        const ret = wasm.temperature_get_degree(this.__wbg_ptr);
+        return ret;
+    }
+    /**
+    * Set the temparature's degree
+    * @param {number} degree
+    */
+    set_degree(degree) {
+        wasm.temperature_set_degree(this.__wbg_ptr, degree);
+    }
+    /**
+    * @returns {Unit}
+    */
+    get_from_unit() {
+        const ret = wasm.temperature_get_from_unit(this.__wbg_ptr);
+        return ret;
+    }
+    /**
+    * Set the temparature's from unit
+    * @param {Unit} unit
+    */
+    set_from_unit(unit) {
+        wasm.temperature_set_from_unit(this.__wbg_ptr, unit);
+    }
+    /**
+    * @returns {Unit}
+    */
+    get_to_unit() {
+        const ret = wasm.temperature_get_to_unit(this.__wbg_ptr);
+        return ret;
+    }
+    /**
+    * Set the temparature's to unit
+    * @param {Unit} unit
+    */
+    set_to_unit(unit) {
+        wasm.temperature_set_to_unit(this.__wbg_ptr, unit);
+    }
 }
+
+export function __wbindgen_object_drop_ref(arg0) {
+    takeObject(arg0);
+};
+
+export function __wbindgen_string_new(arg0, arg1) {
+    const ret = getStringFromWasm0(arg0, arg1);
+    return addHeapObject(ret);
+};
+
+export function __wbg_new_abda76e883ba8a5f() {
+    const ret = new Error();
+    return addHeapObject(ret);
+};
+
+export function __wbg_stack_658279fe44541cf6(arg0, arg1) {
+    const ret = getObject(arg1).stack;
+    const ptr1 = passStringToWasm0(ret, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+    const len1 = WASM_VECTOR_LEN;
+    getInt32Memory0()[arg0 / 4 + 1] = len1;
+    getInt32Memory0()[arg0 / 4 + 0] = ptr1;
+};
+
+export function __wbg_error_f851667af71bcfc6(arg0, arg1) {
+    let deferred0_0;
+    let deferred0_1;
+    try {
+        deferred0_0 = arg0;
+        deferred0_1 = arg1;
+        console.error(getStringFromWasm0(arg0, arg1));
+    } finally {
+        wasm.__wbindgen_free(deferred0_0, deferred0_1, 1);
+    }
+};
+
+export function __wbg_log_5bb5f88f245d7762(arg0) {
+    console.log(getObject(arg0));
+};
 
 export function __wbindgen_throw(arg0, arg1) {
     throw new Error(getStringFromWasm0(arg0, arg1));
