a15d6353b0a3d2ec2e84cf9fb264fa83ff710c61
Prince Muel <vansomecsam@gmail.com>
2024-07-05 16:06:20 +0100

    feat: update assets paths

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,2 +1,7 @@
-import "./main.css";
-import "htmx.org";
+// A dependency graph that contains any wasm must all be imported
+// asynchronously. This `main.js` file does the single async import, so
+// that no one else needs to worry about it again.
+import "./assets/global.css";
+import("./scripts/index.ts").catch((e) =>
+  console.error("Error importing `index.ts`:", e)
+);
