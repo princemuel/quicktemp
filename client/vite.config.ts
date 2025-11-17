26bb078e3df221bfc3a3e46e5cda22f5fc98122f
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-11-17 04:06:39 +0100

    chore: migrate to Tailwindcss v4

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,8 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
-  plugins: [wasm(), topLevelAwait()],
-  server: { port: 3000 },
+    plugins: [tailwindcss(), wasm()],
+    build: { target: "esnext" },
 });
